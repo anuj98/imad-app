07b93dae90ff19a48660f7820d1b0de886775f56
anuj98 <30874550+anuj98@users.noreply.github.com>
2018-02-19 17:53:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 //image cicking functions
 var image = document.getElementById("img");
 image.onClick = function(){
-    image.style.marginLeft = "100px";  
+    image.style.marginLeft = "200px";  
 };
