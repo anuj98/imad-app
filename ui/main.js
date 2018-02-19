da4f7cce9168253314df6ff504d798902465ebd0
anuj98 <30874550+anuj98@users.noreply.github.com>
2018-02-19 18:02:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,7 @@ console.log('Loaded!');
 //image cicking functions
 var image = document.getElementById("img");
 image.onClick = function(){
+    
+    var interval = setInterval(moveLeft, 100);
     image.style.marginLeft = '200px';  
 };
